93b95a41128eb1170ca41e1ca7f9307485127bfd
Dev-Beom <youngbeom0908@gmail.com>
2021-04-08 02:49:50 +0900

    [Update] 클라이언트 케스트를 위한 더미데이터 추가

---
+++ lib/service/GasData.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+import 'package:fl_chart/fl_chart.dart';
+
+class GasData {
+  GasData();
+
+  List<FlSpot> _spotData = <FlSpot>[];
+
+  List<FlSpot> getDay() {
+    _spotData.clear();
+    Random random = new Random();
+    for (var i = 1; i <= 12; i++) {
+      _spotData.add(FlSpot(i.toDouble(), random.nextDouble()));
+    }
+    return _spotData;
+  }
+
+  List<FlSpot> getWeek() {
+    _spotData.clear();
+    Random random = new Random();
+    for (var i = 1; i <= 7; i++) {
+      _spotData.add(FlSpot(i.toDouble(), random.nextDouble()));
+    }
+    return _spotData;
+  }
+
+  List<FlSpot> getMonth() {
+    _spotData.clear();
+    Random random = new Random();
+    for (var i = 1; i <= 30; i++) {
+      _spotData.add(FlSpot(i.toDouble(), random.nextDouble()));
+    }
+    return _spotData;
+  }
+
+  List<FlSpot> getYear() {
+    _spotData.clear();
+    Random random = new Random();
+    for (var i = 1; i <= 12; i++) {
+      _spotData.add(FlSpot(i.toDouble(), random.nextDouble()));
+    }
+    return _spotData;
+  }
+}
