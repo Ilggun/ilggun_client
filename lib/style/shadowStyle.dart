ced0918fc0e62f551e093fa42fb6f0fd6ca6bf70
Dev-Beom <youngbeom0908@gmail.com>
2021-05-27 23:34:43 +0900

    [Update] 그림자 스타일 제거

--- lib/style/shadowStyle.dart
+++ lib/style/shadowStyle.dart
@@ -5,7 +5,7 @@ class ShadowStyle {
   static List<BoxShadow> shadow() {
     List<BoxShadow> shadow = [
       BoxShadow(
-        color: Colors.black.withOpacity(0.09),
+        color: Colors.black.withOpacity(0.00),
         spreadRadius: 2,
         blurRadius: 5,
         offset: Offset(0, 3),
